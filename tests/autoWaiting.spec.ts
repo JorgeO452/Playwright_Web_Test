87c39222598c462bbf1bcd8e6eead81daaedb091
Jorge <jota58@hotmail.com>
2024-06-27 11:33:17 -0500

    Feature: Environment Variables :sparkles:

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import {test, expect} from '@playwright/test'
 
-test.beforeEach(async({page}) => { 
-    await page.goto('http://uitestingplayground.com/ajax')
+test.beforeEach(async({page}, testInfo) => { 
+    await page.goto(process.env.URL)
     await page.getByText('Button Triggering AJAX Request').click()
+    testInfo.setTimeout(testInfo.timeout + 2000)
 })
 
 test('Auto waiting', async ({page}) => {
